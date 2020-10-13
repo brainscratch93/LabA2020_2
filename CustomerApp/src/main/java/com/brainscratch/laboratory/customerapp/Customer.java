aff2357a06e4b7ceade0c7e3b709934726810844
manuelmartorana <brainscratch@live.it>
2020-10-13 23:02:32 +0200

    created json files on customerapp repo

--- CustomerApp/src/main/java/com/brainscratch/laboratory/customerapp/Customer.java
+++ CustomerApp/src/main/java/com/brainscratch/laboratory/customerapp/Customer.java
@@ -1,8 +1,68 @@
 package com.brainscratch.laboratory.customerapp;
 
 public class Customer {
-    public static void main(String[] args) {
-        System.out.println("customer");
+
+    private String name;
+    private String lastName;
+    private String city;
+    private String residenceInitials;
+    private String email;
+    private String nickname;
+    private String password;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getResidenceInitials() {
+        return residenceInitials;
+    }
+
+    public void setResidenceInitials(String residenceInitials) {
+        this.residenceInitials = residenceInitials;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getNickname() {
+        return nickname;
+    }
+
+    public void setNickname(String nickname) {
+        this.nickname = nickname;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
     }
 }
-
---
+++ CustomerApp/src/main/java/com/brainscratch/laboratory/customerapp/Json.java
@@ -0,0 +1,45 @@
+package com.brainscratch.laboratory.customerapp;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import java.io.File;
+import java.io.IOException;
+
+public class Json {
+    public static void main(String[] args) {
+
+        ObjectMapper mapper = new ObjectMapper();
+
+        Customer customer = createCustomer();
+
+        try {
+
+            // Java objects to JSON file
+            mapper.writeValue(new File("c:\\test\\customer.json"), customer);
+
+            // Java objects to JSON string - compact-print
+            String jsonString = mapper.writeValueAsString(customer);
+
+            System.out.println(jsonString);
+            // Java objects to JSON string - pretty-print
+            String jsonInString2 = mapper.writerWithDefaultPrettyPrinter().writeValueAsString(customer);
+            System.out.println(jsonInString2);
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private static Customer createCustomer() {
+        Customer customer = new Customer();
+
+        customer.setName("Alan");
+        customer.setLastName("Brit");
+        customer.setCity("Rescaldina");
+        customer.setResidenceInitials("MI");
+        customer.setEmail("illoallo@gmail.com");
+        customer.setNickname("illoallo");
+        customer.setPassword("...");
+
+        return customer;
+    }
+}
